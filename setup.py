591dd1057f23d3bb9b0f93645f99b63553927ac6
Benjamin Wild <nimajnebwild@gmail.com>
2015-05-21 15:13:42 +0200

    add project scaffolding

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='drunken-markov',
+      version='0.1',
+      description='Markov chain analysis / estimation pipeline',
+      url='https://github.com/nebw/drunken-markov',
+      packages=['drunkenmarkov'],
+      zip_safe=False)
