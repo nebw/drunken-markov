c15416f435436678998aa2d668bf77de9cd19234
dibakma <dibakma@zedat.fu-berlin.de>
2015-05-22 08:36:38 +0200

    added msm class and configured timescales to be a method of msm.

--- drunkenmarkov/Analysis.py
+++ drunkenmarkov/Analysis.py
@@ -2,48 +2,56 @@
 import numpy as np
 from pyemma.msm.analysis import eigenvalues
 
+class msm:
+    def __init__(self, T, lagtime=1.):
+        self.T = T
+	    # compute eigenvalues (function to be exchanged by our own...)
+        self.eigenv = eigenvalues(T)  
+        self.lagtime = lagtime
+        #only compute the timescales if they are called explicitly. This might not be necessary here, but can be useful at some other point of the project.
+        self._timescales = None
 
-def timescales(T, lagtime=1.):
-	"""
-	Compute the time scales of a given transition matrix T.
+    @property
+    def timescales(self):
+        """
+        Compute the time scales of a given transition matrix T.
 
-	Keyword arguments:
-	lagtime tau (default 1.0)
-	"""
+        Keyword arguments:
+        lagtime tau (default 1.0)
+        """        
+        if self._timescales is None:
+            # test for complex eigenvalues
+            ev_is_cmplx = np.where(np.imag(self.eigenv) > 0.)
+            if sum(ev_is_cmplx) > 0: 
+                print 'Complex eigenvalues found!'
 
-	# compute eigenvalues (function to be exchanged by our own...)
-	eigenv = eigenvalues(T)
+            re_eigenv = np.real(self.eigenv)
+            # continue with real part only
+            self._timescales = np.zeros_like(re_eigenv)
 
-	# test for complex eigenvalues
-	ev_is_cmplx = np.where(np.imag(eigenv) > 0.)
-	if sum(ev_is_cmplx) > 0: print 'Complex eigenvalues found!'
-	eigenv = np.real(eigenv)
+            # take care of devision by zero (EV = 1) and compute time scales
+            # for loop to be replaced by something faster
+            for ii in range(len(re_eigenv)):
+    	        if (re_eigenv[ii] - 1.)**2 < 1e-5:
+    		        self._timescales[ii] = np.inf
+    	        else:
+    		        self._timescales[ii] = -self.lagtime / np.log(abs(re_eigenv[ii]))
 
-	# continue with real part only
-	timescales = np.zeros_like(eigenv)
-
-	# take care of devision by zero (EV = 1) and compute time scales
-	# for loop to be replaced by something faster
-	for ii in range(len(eigenv)):
-		if (eigenv[ii] - 1.)**2 < 1e-5:
-			timescales[ii] = np.inf
-		else:
-			timescales[ii] = -lagtime / np.log(abs(eigenv[ii]))
-
-	return timescales
+        return self._timescales
 
 # Sorry, das ist am falschen Ort, aber bei dummy_test.py gibt's bei
 # mir nen Fehler (import drunkenmarkov fehlgeschlagen). (Tim)
 
 def test_timescales_simple():
-	"""
-	Tests if the implemented timescales method (analysis.timescales) works
-	with a simple transition matrix (taken from http://www.pythonhosted.org/
-	pyEMMA/api/generated/pyemma.msm.analysis.timescales.html)
-	"""
-	T = np.array([[0.9, 0.1, 0.0], [0.5, 0.0, 0.5], [0.0, 0.1, 0.9]])
+    """
+    Tests if the implemented timescales method (analysis.timescales) works
+    with a simple transition matrix (taken from http://www.pythonhosted.org/
+    pyEMMA/api/generated/pyemma.msm.analysis.timescales.html)
+    """
+    T = np.array([[0.9, 0.1, 0.0], [0.5, 0.0, 0.5], [0.0, 0.1, 0.9]])
+    
+    markovmodel = msm(T)
+    comp_timescales = markovmodel.timescales
+    ref_timescales = np.array([np.inf, 9.49122158, 0.43429448])
 
-	comp_timescales = timescales(T)
-	ref_timescales = np.array([np.inf, 9.49122158, 0.43429448])
-
-	return np.allclose(comp_timescales, ref_timescales, rtol=1.e-5)
+    return np.allclose(comp_timescales, ref_timescales, rtol=1.e-5)
