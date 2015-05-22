a9fbeb714df2a2c9f573d1d32857bf6cd718c8ac
Benjamin Wild <b.w@fu-berlin.de>
2015-05-22 11:54:08 +0200

    Fix whitespace

--- drunkenmarkov/Analysis.py
+++ drunkenmarkov/Analysis.py
@@ -2,13 +2,16 @@
 import numpy as np
 from pyemma.msm.analysis import eigenvalues
 
+
 class msm:
     def __init__(self, T, lagtime=1.):
         self.T = T
-	    # compute eigenvalues (function to be exchanged by our own...)
-        self.eigenv = eigenvalues(T)  
+        # compute eigenvalues (function to be exchanged by our own...)
+        self.eigenv = eigenvalues(T)
         self.lagtime = lagtime
-        #only compute the timescales if they are called explicitly. This might not be necessary here, but can be useful at some other point of the project.
+        # only compute the timescales if they are called explicitly.
+        # This might not be necessary here, but can be useful at some
+        # other point of the project.
         self._timescales = None
 
     @property
@@ -18,12 +21,12 @@ class msm:
 
         Keyword arguments:
         lagtime tau (default 1.0)
-        """        
+        """
         if self._timescales is None:
             # test for complex eigenvalues
             ev_is_cmplx = np.where(np.imag(self.eigenv) > 0.)
-            if sum(ev_is_cmplx) > 0: 
-                print 'Complex eigenvalues found!'
+            if sum(ev_is_cmplx) > 0:
+                print('Complex eigenvalues found!')
 
             re_eigenv = np.real(self.eigenv)
             # continue with real part only
@@ -32,15 +35,16 @@ class msm:
             # take care of devision by zero (EV = 1) and compute time scales
             # for loop to be replaced by something faster
             for ii in range(len(re_eigenv)):
-    	        if (re_eigenv[ii] - 1.)**2 < 1e-5:
-    		        self._timescales[ii] = np.inf
-    	        else:
-    		        self._timescales[ii] = -self.lagtime / np.log(abs(re_eigenv[ii]))
+                if (re_eigenv[ii] - 1.)**2 < 1e-5:
+                    self._timescales[ii] = np.inf
+                else:
+                    self._timescales[ii] = -self.lagtime / np.log(abs(re_eigenv[ii]))
 
         return self._timescales
 
 # Sorry, das ist am falschen Ort, aber bei dummy_test.py gibt's bei
 # mir nen Fehler (import drunkenmarkov fehlgeschlagen). (Tim)
+
 
 def test_timescales_simple():
     """
@@ -49,7 +53,7 @@ def test_timescales_simple():
     pyEMMA/api/generated/pyemma.msm.analysis.timescales.html)
     """
     T = np.array([[0.9, 0.1, 0.0], [0.5, 0.0, 0.5], [0.0, 0.1, 0.9]])
-    
+
     markovmodel = msm(T)
     comp_timescales = markovmodel.timescales
     ref_timescales = np.array([np.inf, 9.49122158, 0.43429448])
