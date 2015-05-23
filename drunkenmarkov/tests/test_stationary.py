f884aeba13090fa91e905a76a72a6905c8b5a0da
susaro <susanne.roehl@fu-berlin.de>
2015-05-23 13:08:55 +0200

    test_stationary correction

--- drunkenmarkov/tests/test_stationary.py
+++ drunkenmarkov/tests/test_stationary.py
@@ -17,4 +17,4 @@ class StationaryTests(TestCase):
 
         markovmodel = msm(T)
         pi = markovmodel.stationary_distribution
-        self.assertEqual((pi*T, pi).all())
+        self.assertTrue(np.allclose(pi*T,pi,rtol=1e-05, atol=1e-08))
