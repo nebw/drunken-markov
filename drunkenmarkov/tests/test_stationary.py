5fd657e57e98339ffda0cd75d45b5ea12ed2b2da
Tim Hempel <thempel@zedat.fu-berlin.de>
2015-05-26 17:27:32 +0200

    Fixed stationary distribution test with the right matrix multiplication.

--- drunkenmarkov/tests/test_stationary.py
+++ drunkenmarkov/tests/test_stationary.py
@@ -6,15 +6,16 @@ from unittest import TestCase
 
 from drunkenmarkov.Analysis import msm
 
+
 class StationaryTests(TestCase):
     def test_stationary(self):
         """
-        Tests if the implemented timescales method (analysis.timescales) works
-        with a simple transition matrix (taken from http://www.pythonhosted.org/
-        pyEMMA/api/generated/pyemma.msm.analysis.timescales.html)
+        Tests if the implemented stationary distribution method
+        (analysis.stationary_distribution) works with a simple
+        transition matrix.
         """
         T = np.array([[0.9, 0.1, 0.0], [0.5, 0.0, 0.5], [0.0, 0.1, 0.9]])
 
         markovmodel = msm(T)
         pi = markovmodel.stationary_distribution
-        self.assertTrue(np.allclose(pi*T,pi,rtol=1e-05, atol=1e-08))
+        self.assertTrue(np.allclose(np.dot(pi, T), pi, rtol=1e-05, atol=1e-08))
