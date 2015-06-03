fc30ec486c52171565f29c5b2c235da5a49ac8cc
Tim Hempel <thempel@zedat.fu-berlin.de>
2015-06-03 14:18:53 +0200

    Added simple backward committor unity test.

--- drunkenmarkov/tests/TPT_test.py
+++ drunkenmarkov/tests/TPT_test.py
@@ -8,7 +8,6 @@ from unittest import TestCase
 from drunkenmarkov.Analysis import TransitionPathTheory
 
 
-
 class TPTTests(TestCase):
     def test_fcom_simple(self):
         """
@@ -17,11 +16,27 @@ class TPTTests(TestCase):
         in early 2015 at FU Berlin.
         """
 
-        rate_matrix = np.array([[-1, 1, 0],[100, -300, 200],[0, 1, -1]])
+        rate_matrix = np.array([[-1, 1, 0], [100, -300, 200], [0, 1, -1]])
         K = scipy.linalg.expm(rate_matrix)
         TPT = TransitionPathTheory(K, 0, 1)
 
         fcom = TPT.fcom
         ref_fcom = np.array([0, 1., 0.0156032])
-        
+
         self.assertTrue(np.allclose(fcom, ref_fcom, rtol=1.e-5))
+
+    def test_bcom_simple(self):
+        """
+        Tests the implemented backward committor function bcom.
+        Example taken from the pyEMMA workshop by the Noe group
+        in early 2015 at FU Berlin.
+        """
+
+        rate_matrix = np.array([[-1, 1, 0], [100, -300, 200], [0, 1, -1]])
+        K = scipy.linalg.expm(rate_matrix)
+        TPT = TransitionPathTheory(K, 0, 1)
+
+        bcom = TPT.bcom
+        ref_bcom = np.array([1.,  0., 0.9843968])
+
+        self.assertTrue(np.allclose(bcom, ref_bcom, rtol=1.e-5))
