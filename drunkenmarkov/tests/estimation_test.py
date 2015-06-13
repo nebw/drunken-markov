282f18cd44e630e78040021657a53e5f1d263c74
Tim Hempel <thempel@zedat.fu-berlin.de>
2015-06-13 12:16:48 +0200

    Added test for count matrix function

--- drunkenmarkov/tests/estimation_test.py
+++ drunkenmarkov/tests/estimation_test.py
@@ -2,8 +2,8 @@
 
 import numpy as np
 from unittest import TestCase
-import drunkenmarkov
-from drunkenmarkov.Estimation import tmatrix
+from drunkenmarkov.Estimation import tmatrix, cmatrix
+
 
 class TMatrixTest(TestCase):
     def test_TMatrix_rowsums(self):
@@ -11,7 +11,7 @@ class TMatrixTest(TestCase):
         Tests if the row sums of a random 10x10 transition matrix yield one.
         """
 
-        C = np.random.randint(1000, size=(10,10))
+        C = np.random.randint(1000, size=(10, 10))
         T = tmatrix(C)
         T_rowsums = T.sum(axis=1)
 
@@ -26,3 +26,15 @@ class TMatrixTest(TestCase):
         T_analytical = np.array([[.9, .1], [.7, .3]])
 
         self.assertTrue(np.allclose(T_computed, T_analytical))
+
+
+class CMatrixTest(TestCase):
+    def test_cmatrix_simple(self):
+        """
+        Simple count matrix test.
+        """
+
+        test_dtraj = np.array([0, 1, 1, 0, 0, 0, 1, 1, 1, 1])
+        cmatrix_compare = np.array([[2., 2.], [1., 4.]])
+        cmatrix_computed = cmatrix(test_dtraj)
+        self.assertTrue(np.allclose(cmatrix_compare, cmatrix_computed))
