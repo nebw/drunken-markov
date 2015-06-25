c155df5f20899048e8321d10d04997a4eb3fea69
David Dormagen <czapper@gmx.de>
2015-06-25 10:36:09 +0200

    kmeans: added softer stopping criterion (introduced standard patience approach and allowed setting a threshold of changes that are required to continue fitting)

--- drunkenmarkov/clustering/kmeans.py
+++ drunkenmarkov/clustering/kmeans.py
@@ -13,13 +13,22 @@ class KMeans(object):
 	# the final tree of the clustering algorithm
 	_kdtree = None
 	
+	# stopping criterion
+	# how many iterations are allowed with a threshold of change below _patience_threshold?
+	_patience = 10
+	# the ratio of changes in the assignments necessary to continue the fitting;
+	# set to 0 to disable early stopping
+	_patience_threshold = 0.1
+	
 	# creates a new k-means clustering object with a specified k
-	def __init__(self, k = None):
+	def __init__(self, k = None, _patience = None, _patience_threshold = None):
 		# sanity checks
 		if k is None or k <= 0:
 			raise ValueError("k needs to be a positive integer.")
 			
 		self.k = k
+		self._patience = _patience or self._patience
+		self._patience_threshold = _patience_threshold or self._patience_threshold 
 	
 	# discretizes data points with the previously calculated cluster centers
 	def discretise(self, data):
@@ -39,20 +48,31 @@ class KMeans(object):
 		current_cluster_centers = data[np.random.choice(data_length, size=self.k, replace=False), :]
 
 		# repeat the loop until the cluster assignments do not change anymore
-		at_least_one_assignment_changed = True
+		stopping_criterion_met = False
+		current_patience = self._patience
 		
-		while at_least_one_assignment_changed:
+		while not stopping_criterion_met:
 			# consult a KD tree for performance improvement
 			cluster_center_tree = KDTree(current_cluster_centers, leafsize=5)
 			_, data_membership  = cluster_center_tree.query(data)
 
 			# update stopping condition
-			# manual loop instead of equality operator as the latter always checks all elements, ...
-			at_least_one_assignment_changed = False
-			for i in range(data_length):
-				if cluster_map[i] == data_membership[i]: continue
-				at_least_one_assignment_changed = True
-				break
+			if self._patience_threshold > 0.0:
+				# calculate ratio of changes and apply threshold & patience
+				same_assignment_count = np.sum(cluster_map == data_membership)
+				change_ratio  = 1.0 - same_assignment_count / float(data_length)
+				if change_ratio < self._patience_threshold:
+					current_patience -= 1
+					if current_patience <= 0:
+						stopping_criterion_met = True
+			else:
+				# check whether there is at least ONE assignment that did not change
+				# manual loop instead of equality operator as the latter always checks all elements, ...
+				stopping_criterion_met = True
+				for i in range(data_length):
+					if cluster_map[i] == data_membership[i]: continue
+					stopping_criterion_met = False
+					break
 			
 			# and finally update the cluster centers to be the centroids of the voronoi subsets
 			cluster_map = data_membership
