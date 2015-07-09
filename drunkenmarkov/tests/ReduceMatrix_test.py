13d4eb2cb4ec5c47d77fcd305eb85bbbe8754d71
Tim Hempel <thempel@zedat.fu-berlin.de>
2015-07-09 20:17:10 +0200

    Python3 exception for ReduceMatrixTest (PyEMMA)

--- drunkenmarkov/tests/ReduceMatrix_test.py
+++ drunkenmarkov/tests/ReduceMatrix_test.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 
 import numpy as np
-
 from unittest import TestCase
-
 from drunkenmarkov.Analysis import MarkovStateModel
+import sys
 
+def onlypython2(fun):
+    """
+    This is a decorator that effectively disables a unit test when using
+    python 2 by replacing the test function with a lambda function that always
+    returns True.
 
+    pyEMMA is not available for python 3. Therefore we have to disable tests
+    based on pyEMMA when running python 3.
+    """
+    if sys.version_info >= (3, 0):
+        return lambda: True
+    else:
+        return fun
+
+@onlypython2
 class ReduceMatrixTests(TestCase):
     def test_ReduceMatrixRowsum(self):
         """
