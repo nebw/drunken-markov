58780ae367e0bd5822e6c7e2792607b0c48aba3b
Manuel Dibak <dibakma@zedat.fu-berlin.de>
2015-07-10 01:20:41 +0200

    added test for tica class

--- drunkenmarkov/tests/test_pca.py
+++ drunkenmarkov/tests/test_pca.py
@@ -4,27 +4,28 @@ import drunkenmarkov.Clustering
 import sys
 
 def onlypython2(fun):
-    """
-    This is a decorator that effectively disables a unit test when using
-    python 2 by replacing the test function with a lambda function that always
-    returns True.
+	"""
+	This is a decorator that effectively disables a unit test when using
+	python 2 by replacing the test function with a lambda function that always
+	returns True.
 
-    pyEMMA is not available for python 3. Therefore we have to disable tests
-    based on pyEMMA when running python 3.
-    """
-    if sys.version_info >= (3, 0):
-        return lambda: True
-    else:
-        return fun
+	pyEMMA is not available for python 3. Therefore we have to disable tests
+	based on pyEMMA when running python 3.
+	"""
+	if sys.version_info >= (3, 0):
+		return lambda: True
+	else:
+		return fun
 
 if sys.version_info < (3, 0):
-    from pyemma.coordinates import pca as emma_pca
+	from pyemma.coordinates import pca as emma_pca
+	from pyemma.coordinates import tica as emma_tica
 
 @onlypython2
-class pcaTests(TestCase):
+class pca_tica_Tests(TestCase):
 	def test_pca(self):
 		"""
-		Testing the implemented PCA class against the pyemma result
+		Testing the implemented PCA and tica class with the respective pyemma result
 		Create deterministic data in [1,1] direction with noise in perpendiculate [1,-1] direction and apply pca
 		"""
 		data = np.zeros((1000,2))
@@ -36,3 +37,11 @@ class pcaTests(TestCase):
 		emmapca = emma_pca(data)
 		self.assertTrue(np.allclose(pca.reduced_data(L = 2),emmapca.get_output()))
 		self.assertTrue(np.allclose(pca.covariance, emmapca.covariance_matrix))
+
+		data2 = np.random.rand(1000,3)
+		data2 = np.dot(data2, np.diag([1,5,10]))
+		drunkentica = drunkenmarkov.tica(data2)
+		emmatica = emma_tica(data2, lag = 10, dim = 3)
+		emmadata = emmatica.get_output()
+		drunkendata = drunkentica.reduced_data()
+		self.assertTrue(np.allclose(np.absolute(drunkendata),np.absolute(emmadata), atol = 1e-03))
